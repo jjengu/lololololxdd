893c0f4bf945519a1d14b3e7366a93b54fe4b0e0
jengu <124980843+jjengu@users.noreply.github.com>
2025-09-23 21:20:45 -0400

    Delete pages/api directory

--- pages/api/ip.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const ip =
-    req.headers['x-forwarded-for']?.split(',')[0] ||
-    req.socket?.remoteAddress ||
-    'Unknown IP';
-
-  console.log('Visitor IP:', ip);
-  res.status(200).json({ ip });
-}
